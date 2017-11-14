a70c5de4cf274abe9fea7f07bf7e66dfac3772e0
Jesús Valdez <alonso6230@gmail.com>
2017-11-14 08:18:33 -0400

    new GDAX websocket client

---
+++ core/GDAX_data_feeder.py
@@ -0,0 +1,42 @@
+import logging
+import json
+import sys
+import os
+import time
+import websocket
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from core.libraries.pub_sub import Publisher, Subscriber
+from core.libraries.websocket_thread import ConnectThread
+from core.libraries.channels import channels as ch
+
+class GDAXWebSocketClient():
+
+    def __init__(self,data):
+
+        websocket.enableTrace(True)
+        self.ws = websocket.WebSocketApp("wss://ws-feed.gdax.com",
+                                        on_message=self.on_message,
+                                        on_open=self.on_open)
+        self.sub = json.dumps(data)
+        self.ws.op_open = self.on_open
+        self.ws.run_forever()
+    def on_message(self, ws, message):
+
+        types = json.loads(message)['type']
+        if types == "received" or types == "open":
+            print(message)
+
+    def on_open(self, ws):
+        ws.send(self.sub)
+
+def main():
+    pairs=["ETH-USD","BTC-USD"]
+
+    request={"type": "subscribe", "channels": [{ "name": "full",
+            "product_ids": pairs }] }
+
+    GDAXWebSocketClient(request)
+
+if __name__=="__main__":
+    main()
